790d3cf6163051655096ff160355d602f7f14897
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-05-28 15:23:53 +0200

    okay

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,4 @@
 class Actor < User
   belongs_to :user
+  validates :name, :surname, :age, :race, :gender, :location, presence: true
 end
--- app/models/production_company.rb
+++ app/models/production_company.rb
@@ -1,3 +1,6 @@
 class ProductionCompany < User
   belongs_to :user
+    validates :title, :address, presence: true, uniqueness: true
+    validates :postal_code, :address, :city, :province, presence: true
+
 end
