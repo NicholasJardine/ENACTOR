1b18e72971110e7ec0cc932e2fcc5a6464b48273
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-07-03 12:29:05 +0200

    coming together

--- app/models/brief.rb
+++ app/models/brief.rb
@@ -1,7 +1,7 @@
 class Brief < ApplicationRecord
   has_many :auditions
-  has_many :user_briefs
-  has_many :users, through: :user_briefs
+  # has_many :user_briefs
+  belongs_to :user
   validates :name, :age, :ethnicty, :gender, presence: true
 
    mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_one_attached :photo
   has_many :user_briefs
-  has_many :briefs, through: :user_briefs
-  has_many :user_auditions
-  has_many :auditions, through: :user_auditions
+  has_many :briefs
+  # has_many :user_auditions
+  has_many :auditions
 
 
   # Include default devise modules. Others available are:
