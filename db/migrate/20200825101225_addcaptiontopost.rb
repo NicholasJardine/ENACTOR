0fe678b19255b9ba1f3db1f2758468885c1145b7
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-08-25 12:21:01 +0200

    caption for posts is a go

---
+++ db/migrate/20200825101225_addcaptiontopost.rb
@@ -0,0 +1,5 @@
+class Addcaptiontopost < ActiveRecord::Migration[5.2]
+  def change
+    add_column :posts, :caption, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_123403) do
+ActiveRecord::Schema.define(version: 2020_08_25_101225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,6 +140,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_123403) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.text "caption"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
