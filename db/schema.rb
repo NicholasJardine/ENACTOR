8bd2f74d7c13eb3617339fa6a34178a948350676
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-09-22 17:07:17 +0200

    attached

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_095724) do
+ActiveRecord::Schema.define(version: 2020_09_22_133002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -299,6 +299,7 @@ ActiveRecord::Schema.define(version: 2020_09_22_095724) do
     t.text "contact_details"
     t.boolean "accepted_terms", default: false
     t.string "cv"
+    t.string "ciricv"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
