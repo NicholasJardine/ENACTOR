d637b70ae589169e5b19fa56ce5cbc9bf26782c3
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-10-01 13:47:04 +0200

    concept of companies home page started

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,14 @@ class PagesController < ApplicationController
     @articles = Article.all
     @articles_without_latest = @articles.reject{|article| article == @last}
 
+
+    if params[:query].present?
+      sql_query = " \
+        users.name @@ :query "
+      @users = User.where(sql_query, query: "%#{params[:query]}%").where(occupation: "Actor")
+    else
+      @users = User.where(occupation: "Actor")
+    end
     # @others = @articles.drop(@articles.length - 1)
   end
 
