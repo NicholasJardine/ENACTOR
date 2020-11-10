3890efa7aba3710835a645ec98faf4bc8401fc5b
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-11-10 15:11:34 +0200

    users show looks cool

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,12 @@ class UsersController < ApplicationController
     @u = User.find(params[:id])
     @private_brief = PrivateBrief.new
     @posts = Post.where(user_id: @u.id)
+  @my_posts = Post.where(user_id: @user.id)
 
   end
+
+
+
 
 def destroy
   @u = User.find(params[:id])
